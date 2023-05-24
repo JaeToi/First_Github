49e5feeddc8d5b32009366861760b5a7c208a38d
woehd1019 <woehd1019@naver.com>
2023-05-24 16:58:30 +0900

    first commit

---
+++ src/main/java/org/zerock/sample/Chef.java
@@ -0,0 +1,13 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component // 재사용이 가능한 컴포넌트 설정
+@Data // setter를 생성하는기능과 생성자,toString()등을 자동으로 생성하는 것
+public class Chef {
+	
+	
+	
+}
